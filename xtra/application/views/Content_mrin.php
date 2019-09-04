5e6e06fe9fc057cdab2fe1e6547c0df5dce608ab
faiz <muhammadfaiznoh@gmail.com>
2019-09-04 11:07:11 +0800

    mobile design optimization
    improve design for mobile and tablet view

--- xtra/application/views/Content_mrin.php
+++ xtra/application/views/Content_mrin.php
@@ -214,24 +214,24 @@
 									}
 								?>
 								<?php $rownum = 1; foreach ($record as $row): ?>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td >No</td>
-								<td class="td-desk">: <?=$rownum;?></td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">No</td>
+								<td class="td-desk">: <?=$rownum++;?></td>
 							</tr>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>MRIN Reference No</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">MRIN Reference No</td>
 								<td class="td-desk">:
 									<a href="<?php echo base_url();?>index.php/Procurement?mrinno=<?=$row->DocReferenceNo?>&pro=<?=$pro?>">
 										<?=isset($row->DocReferenceNo) ? $row->DocReferenceNo : ''?>
 									</a><b></b>
 								</td>
 							</tr>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>Work Order</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">Work Order</td>
 								<td class="td-desk">: <?=isset($row->WorkOfOrder) ? $row->WorkOfOrder : ''?></td>
 							</tr>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>Asset</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">Asset</td>
 								<td class="td-desk">: <?=isset($row->V_Asset_no) ? $row->V_Asset_no : ''?></td>
 							</tr>
 								<?php foreach($status as $stat){ ?>
@@ -254,8 +254,8 @@
 										}
 									?>
 									<?php if ($r_status == $stat->StatusID) { ?>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>Status</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">Status</td>
 								<td class="td-desk">
 									<a rel="nofollow" title="Manager : <?=$s_AM?> <?php if ($s_AM != "Pending") { ?> ON <?=isset($row->DateApproval) ? date("d/m/Y H:i:s",strtotime($row->DateApproval)) : ''?>  <?php } ?> &#13;Procument : <?=$s_Proc?> <?php if ($s_Proc != "Pending") { ?> ON <?=isset($row->DateApprovalx) ? date("d/m/Y H:i:s",strtotime($row->DateApprovalx)) : ''?> &#13; <?=isset($row->DateApprovalxx) ? date("d/m/Y H:i:s",strtotime($row->DateApprovalxx)) : ''?>
 									<?php } ?>">
@@ -267,12 +267,12 @@
 							</tr>
 									<?php } ?>
 								<?php } ?>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>Issue Date</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">Issue Date</td>
 								<td class="td-desk">: <?=isset($row->DateCreated) ? date("d M Y",strtotime($row->DateCreated)) : ''?></td>
 							</tr>
-							<tr <?=($rownum % 2) == 1 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
-								<td>Remark</td>
+							<tr <?=($rownum % 1) == 0 ? 'class="ui-color-color-color"' : 'class="tr_color"'?>>
+								<td class="td-desk">Remark</td>
 								<td class="td-desk">: <?=isset($row->Commentsx) ? $row->Commentsx : ''?></td>
 							</tr>
 								<?php endforeach;?>
